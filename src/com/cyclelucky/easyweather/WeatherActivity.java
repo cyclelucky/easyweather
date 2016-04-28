17fe1c364d82a733593c8ca5479b1601c60b3dcc
qinzongxuan <qinzongxuan@sunniwell.net>
2016-04-28 12:53:10 +0800

    init code

---
+++ src/com/cyclelucky/easyweather/WeatherActivity.java
@@ -0,0 +1,15 @@
+package com.cyclelucky.easyweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class WeatherActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.weather_activity);
+	}
+}
